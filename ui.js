802d83f26623b3cc2e46d6deae278d43c94b01d0
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-12 20:21:58 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,29 @@
+const UI={
 
+render(){
+
+const data=Storage.load();
+
+const balance=Finance.balance(data);
+
+const today=Finance.todayLimit(data);
+
+document.getElementById("app").innerHTML=`
+
+<h1>💰 رصيدي</h1>
+
+<h2>${balance.toFixed(2)} ريال</h2>
+
+<p>
+
+المسموح اليوم
+
+${today} ريال
+
+</p>
+
+`;
+
+}
+
+};
