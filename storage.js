beda7261063e62b343ae02a15d0de2366740edf2
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-12 20:22:55 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,37 @@
+const Storage={
 
+key:"rasidi",
+
+load(){
+
+const data=localStorage.getItem(this.key);
+
+if(!data){
+
+return null;
+
+}
+
+return JSON.parse(data);
+
+},
+
+save(data){
+
+localStorage.setItem(
+
+this.key,
+
+JSON.stringify(data)
+
+);
+
+},
+
+clear(){
+
+localStorage.removeItem(this.key);
+
+}
+
+};
