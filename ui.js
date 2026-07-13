16dd45d98c86b2a23d53809b3c96fab5d42c99a2
om909ar <139689383+om909ar@users.noreply.github.com>
2026-07-13 06:23:36 +0300

    Delete ui.js

--- ui.js
+++
@@ -1,8 +0,0 @@
-const UI = {
-    render() {
-        document.getElementById("app").innerHTML = `
-            <h1>نجح الموقع ✅</h1>
-            <p>إذا تشوف هذي الرسالة فالمشكلة ليست في index أو app.</p>
-        `;
-    }
-};
